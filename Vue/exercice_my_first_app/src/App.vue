5951c7c1ed88a6ab27ece775731a06fa619fa524
Thomas <tborestel@gmail.com>
2024-01-22 13:20:07 +0100

    premier push

--- Vue/exercice_my_first_app/src/App.vue
+++ Vue/exercice_my_first_app/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
+import Component from './assets/Component.vue';
 /*  
 CE COMPOSANT RACINE APP RESTE EN MODE COMPOSITION API
 IMPORTER LES COMPOSANTS ICI 
 */
+
 </script>
 
 <template>
- <h1>OSCU</h1> <!-- Monter les composants ici -->
+  <h1>Ok</h1>
+  <h2>{{ count }}</h2>
+  <Component count='50'/>
 </template>
 
 <style scoped>
---
+++ Vue/exercice_my_first_app/src/assets/Component.vue
@@ -0,0 +1,38 @@
+<script>
+export default { 
+  name : "Component",
+  data () {
+    return{
+      count : 0,
+      message : "Aidez-moi",
+      pfs : true
+    }
+  },
+  props : {
+    msg : {
+      type : String,
+      default : "qqc",
+      required : false
+    }
+  },
+  methods:{
+    add(){
+      this.count++
+    },
+    decrease(){
+      this.count--
+    }
+  }
+}
+</script>
+
+<template>
+  <section>
+    <h2>Hello</h2>
+    <p>{{msg}}</p>
+    <p>{{ message }}</p>
+    <p>{{ count }}</p>
+    <button @:click="add">+</button>
+    <button @:click="decrease">-</button>
+  </section>
+</template>
